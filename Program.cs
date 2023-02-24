9e4366a256d8d93f7d7fef8309bbfcb12c60a186
D-Meshach <meshachdaniel456@gmail.com>
2023-02-25 02:49:29 +0530

    prime factor method added

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace basic_core_day5
             var i = "y";
             while (i == "y")
             {
-                Console.Write("Enter \n 1) for Flip coin  \n 2)for leap year \n 3) power of 2 \n 4) Harmonic Number    \n Enter Option \n\n");
+                Console.Write("Enter \n 1) for Flip coin  \n 2)for leap year \n 3) power of 2 \n 4) Harmonic Number \n 5) for factors    \n Enter Option \n\n");
                 var a = Convert.ToInt32(Console.ReadLine());
                 switch (a)
                 {
@@ -26,6 +26,7 @@ namespace basic_core_day5
                     case 2: leap_year lp = new leap_year(); break;
                     case 3: power2.power_count(); break;
                     case 4: harmonic.harmonic_num(); break;
+                    case 5: factors.factor_find(); break;
                     default: Console.WriteLine("Invalid Option");
                         break;
 
---
+++ factors.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace basic_core_day5
+{
+    internal class factors
+    {
+        //Prime factorization method by using brute force method
+        public static void factor_find() {
+
+            Console.WriteLine("Enter the number greater than 1");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number < 1)
+            {
+
+                Console.WriteLine("Enter number greater than 1");
+
+            }
+            else {
+                int count = 0;
+                Console.WriteLine("The Prime Factors are");
+                for (int i = 1; i <=number; i++) 
+                {
+
+                    if (number % i == 0) {
+                        int tempno = 0;
+                        for (int j = 1; j <= i; j++) {
+                            if (i % j == 0) {
+                                tempno++;
+                               
+                            }
+                        }
+                        if (tempno <=2) {
+                            Console.WriteLine(i);
+                        }
+
+
+                   }
+                    
+
+                }
+               
+            
+            }
+            
+
+
+        }
+    
+
+    }
+}
