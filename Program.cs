091817bcc3974bf0df25986c29110af61b90e51d
D-Meshach <meshachdaniel456@gmail.com>
2023-02-25 02:55:52 +0530

    quotient and remainder added

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace basic_core_day5
             var i = "y";
             while (i == "y")
             {
-                Console.Write("Enter \n 1) for Flip coin  \n 2)for leap year \n 3) power of 2 \n 4) Harmonic Number \n 5) for factors    \n Enter Option \n\n");
+                Console.Write("Enter \n 1) for Flip coin  \n 2)for leap year \n 3) power of 2 \n 4) Harmonic Number \n 5) for factors \n 6)Find quotient and remainder    \n Enter Option \n\n");
                 var a = Convert.ToInt32(Console.ReadLine());
                 switch (a)
                 {
@@ -27,6 +27,7 @@ namespace basic_core_day5
                     case 3: power2.power_count(); break;
                     case 4: harmonic.harmonic_num(); break;
                     case 5: factors.factor_find(); break;
+                    case 6: division.find_quot_rem_div(); break;
                     default: Console.WriteLine("Invalid Option");
                         break;
 
---
+++ division.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_core_day5
+{
+    internal class division
+    {
+
+        //Finding the quotient and remainder
+        public static void find_quot_rem_div() 
+        {
+            Console.WriteLine("Enter the first Number");
+            double num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number");
+            
+            double num2 = Convert.ToInt32(Console.ReadLine());
+            if (num2 > num1)
+            {
+                Console.WriteLine("Enter Numerator Greater than denominator");
+            }
+            else {
+                double rem = num1 % num2;
+                Console.WriteLine("The quotient is " + ((int)(num1 / num2)));
+                Console.WriteLine("The remainder is " + rem);
+            }
+            
+
+
+        }
+
+    }
+}
