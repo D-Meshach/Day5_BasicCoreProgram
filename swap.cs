d9f3c99e39f68c07104e98fcb7fcdde8d3744b0b
D-Meshach <meshachdaniel456@gmail.com>
2023-02-25 03:04:42 +0530

    swap two number added

---
+++ swap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_core_day5
+{
+    public class swap
+    {
+
+        public static void swap_number() {
+
+            int a, b,temp;
+            Console.WriteLine("Enter the value of a");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of b");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before Swapping a=" + a + " and b=" + b);
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After Swapping a=" + a + " and b=" + b);
+
+
+        }
+
+    }
+}
