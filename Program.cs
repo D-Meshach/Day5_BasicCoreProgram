9b21b4346807ce83e15c3846ca2cd28daab980ea
D-Meshach <meshachdaniel456@gmail.com>
2023-02-25 01:57:46 +0530

    Flip Coin Program added

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_core_day5
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = "y";
+            while (i == "y")
+            {
+                Console.Write("Enter \n 1) for Flip coin ");
+                var a = Convert.ToInt32(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the number of times to flip coin"); int cnt = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(cnt);
+                        flip_coin fp = new flip_coin(cnt);
+                        break;
+                    default: Console.WriteLine("Invalid Option");
+                        break;
+
+
+                }
+                Console.WriteLine("Do you wish to continue (y/n)?");
+                i = Console.ReadLine();
+
+                if (i == "n")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ flip_coin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_core_day5
+{
+    public class flip_coin
+    {
+        //flip coin 
+        public flip_coin(int a) {
+            int i = 0,head=0,tail=0;
+            Random random = new Random();
+            while (i < a) {
+               int rand_number= random.Next(0,2);
+                
+                
+                if (rand_number == 1)
+                {
+                    head += 1;
+                }
+                else {
+                    tail += 1;
+                }
+                i++;
+                
+            }
+            double P = (double)(100 / (double)a);
+            double head_percent =(double) P * head;
+            head_percent = Math.Round(head_percent, 1);
+            double tail_percent =(double) P * tail;
+            tail_percent = Math.Round(tail_percent, 1);
+            Console.WriteLine("Head=" + head + "Tail=" + tail);
+            Console.WriteLine("Head Percentage =" +(double) head_percent + "Tail Percentage=" + tail_percent);
+        }
+
+    }
+}
